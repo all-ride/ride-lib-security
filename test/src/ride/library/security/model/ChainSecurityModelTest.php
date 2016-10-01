5931d214f6ee5e6eccde73a651f3c628310411dc
Kaya <kayalion@kayalion.net>
2016-10-01 12:11:41 +0200

    continued chain security model test

--- test/src/ride/library/security/model/ChainSecurityModelTest.php
+++ test/src/ride/library/security/model/ChainSecurityModelTest.php
@@ -17,7 +17,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
     public function testToString() {
         $this->assertEquals('[]', (string) $this->model);
 
-        $modelMock = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
         $modelMock->expects($this->once())
                   ->method('ping')
                   ->will($this->returnValue(true));
@@ -26,7 +27,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals('[' . get_class($modelMock) . ']', (string) $this->model);
 
-        $modelMock2 = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
         $modelMock2->expects($this->once())
                    ->method('ping')
                    ->will($this->returnValue(true));
@@ -37,12 +39,14 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testAddSecurityModel() {
-        $modelMock = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
 
         $this->assertFalse($this->model->addSecurityModel($modelMock));
         $this->assertEquals(array(), $this->model->getSecurityModels());
 
-        $modelMock = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
         $modelMock->expects($this->once())
                   ->method('ping')
                   ->will($this->returnValue(true));
@@ -52,7 +56,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testRemoveSecurityModel() {
-        $modelMock = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
         $modelMock->expects($this->once())
                   ->method('ping')
                   ->will($this->returnValue(true));
@@ -60,7 +65,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
         $this->model->addSecurityModel($modelMock);
         $this->assertEquals(array($modelMock), $this->model->getSecurityModels());
 
-        $modelMock2 = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
 
         $this->assertFalse($this->model->removeSecurityModel($modelMock2));
         $this->assertEquals(array($modelMock), $this->model->getSecurityModels());
@@ -72,7 +78,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
     public function testPing() {
         $this->assertFalse($this->model->ping());
 
-        $modelMock = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
         $modelMock->expects($this->once())
                   ->method('ping')
                   ->will($this->returnValue(true));
@@ -88,7 +95,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
         $path3 = '/path3';
         $path4 = '/path4';
 
-        $modelMock = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
         $modelMock->expects($this->once())
                   ->method('ping')
                   ->will($this->returnValue(true));
@@ -96,7 +104,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
                   ->method('getSecuredPaths')
                   ->will($this->returnValue(array($path1, $path2)));
 
-        $modelMock2 = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
         $modelMock2->expects($this->once())
                    ->method('ping')
                    ->will($this->returnValue(true));
@@ -123,7 +132,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
             '/path2',
         );
 
-        $modelMock = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
         $modelMock->expects($this->once())
                   ->method('ping')
                   ->will($this->returnValue(true));
@@ -132,7 +142,8 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
                   ->with($this->equalTo($securedPaths))
                   ->will($this->returnValue(null));
 
-        $modelMock2 = $this->getMock('ride\\library\\security\\model\\ChainableSecurityModel');
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
         $modelMock2->expects($this->once())
                    ->method('ping')
                    ->will($this->returnValue(true));
@@ -143,6 +154,194 @@ class ChainSecurityModelTest extends PHPUnit_Framework_TestCase {
         $this->model->addSecurityModel($modelMock2);
 
         $this->model->setSecuredPaths($securedPaths);
+    }
+
+    public function testSetGrantedPermissionsToRole() {
+        $permissions = array('permission');
+        $roleMock = $this->getMockBuilder('ride\\library\\security\\model\\Role')
+                         ->getMock();
+
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
+        $modelMock->expects($this->once())
+                  ->method('ping')
+                  ->will($this->returnValue(true));
+        $modelMock->expects($this->once())
+                  ->method('ownsRole')
+                  ->with($this->equalTo($roleMock))
+                  ->will($this->returnValue(false));
+        $modelMock->expects($this->never())
+                  ->method('setGrantedPermissionsToRole');
+
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
+        $modelMock2->expects($this->once())
+                   ->method('ping')
+                   ->will($this->returnValue(true));
+        $modelMock2->expects($this->once())
+                   ->method('ownsRole')
+                   ->with($this->equalTo($roleMock))
+                   ->will($this->returnValue(true));
+        $modelMock2->expects($this->once())
+                   ->method('setGrantedPermissionsToRole')
+                   ->with($this->equalTo($roleMock), $this->equalTo($permissions));
+
+        $this->model->addSecurityModel($modelMock);
+        $this->model->addSecurityModel($modelMock2);
+
+        $this->model->setGrantedPermissionsToRole($roleMock, $permissions);
+    }
+
+    public function testSetAllowedPathsToRole() {
+        $paths = array('/path');
+        $roleMock = $this->getMockBuilder('ride\\library\\security\\model\\Role')
+                         ->getMock();
+
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
+        $modelMock->expects($this->once())
+                  ->method('ping')
+                  ->will($this->returnValue(true));
+        $modelMock->expects($this->once())
+                  ->method('ownsRole')
+                  ->with($this->equalTo($roleMock))
+                  ->will($this->returnValue(false));
+        $modelMock->expects($this->never())
+                  ->method('setAllowedPathsToRole');
+
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
+        $modelMock2->expects($this->once())
+                   ->method('ping')
+                   ->will($this->returnValue(true));
+        $modelMock2->expects($this->once())
+                   ->method('ownsRole')
+                   ->with($this->equalTo($roleMock))
+                   ->will($this->returnValue(true));
+        $modelMock2->expects($this->once())
+                   ->method('setAllowedPathsToRole')
+                   ->with($this->equalTo($roleMock), $this->equalTo($paths));
+
+        $this->model->addSecurityModel($modelMock);
+        $this->model->addSecurityModel($modelMock2);
+
+        $this->model->setAllowedPathsToRole($roleMock, $paths);
+    }
+
+    public function testSetRolesToUser() {
+        $userMock = $this->getMockBuilder('ride\\library\\security\\model\\User')
+                         ->getMock();
+        $roleMock = $this->getMockBuilder('ride\\library\\security\\model\\Role')
+                         ->getMock();
+        $roles = array($roleMock);
+
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
+        $modelMock->expects($this->once())
+                  ->method('ping')
+                  ->will($this->returnValue(true));
+        $modelMock->expects($this->once())
+                  ->method('ownsUser')
+                  ->with($this->equalTo($userMock))
+                  ->will($this->returnValue(false));
+        $modelMock->expects($this->never())
+                  ->method('setRolesToUser');
+
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
+        $modelMock2->expects($this->once())
+                   ->method('ping')
+                   ->will($this->returnValue(true));
+        $modelMock2->expects($this->once())
+                   ->method('ownsUser')
+                   ->with($this->equalTo($userMock))
+                   ->will($this->returnValue(true));
+        $modelMock2->expects($this->once())
+                   ->method('setRolesToUser')
+                   ->with($this->equalTo($userMock), $this->equalTo($roles));
+
+        $this->model->addSecurityModel($modelMock);
+        $this->model->addSecurityModel($modelMock2);
+
+        $this->model->setRolesToUser($userMock, $roles);
+    }
+
+    /**
+     * @dataProvider providerGetUserBy
+     */
+    public function testGetUserBy($method, $query) {
+        $userMock = $this->getMockBuilder('ride\\library\\security\\model\\User')
+                         ->getMock();
+
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
+        $modelMock->expects($this->once())
+                  ->method('ping')
+                  ->will($this->returnValue(true));
+        $modelMock->expects($this->once())
+                  ->method($method)
+                  ->with($this->equalTo($query))
+                  ->will($this->returnValue(null));
+
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
+        $modelMock2->expects($this->once())
+                   ->method('ping')
+                   ->will($this->returnValue(true));
+        $modelMock2->expects($this->once())
+                   ->method($method)
+                   ->with($this->equalTo($query))
+                   ->will($this->returnValue($userMock));
+
+        $this->model->addSecurityModel($modelMock);
+        $this->model->addSecurityModel($modelMock2);
+
+        $result = $this->model->$method($query);
+
+        $this->assertEquals($userMock, $result);
+    }
+
+    public function providerGetUserBy() {
+        return array(
+            array('getUserById', 'id'),
+            array('getUserByUsername', 'username'),
+            array('getUserByEmail', 'e@ma.il'),
+        );
+    }
+
+    public function testGetUsers() {
+
+    }
+
+    public function testCountUsers() {
+        $options = array('query' => 'test');
+
+        $modelMock = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                          ->getMock();
+        $modelMock->expects($this->once())
+                  ->method('ping')
+                  ->will($this->returnValue(true));
+        $modelMock->expects($this->once())
+                  ->method('countUsers')
+                  ->with($this->equalTo($options))
+                  ->will($this->returnValue(2));
+
+        $modelMock2 = $this->getMockBuilder('ride\\library\\security\\model\\ChainableSecurityModel')
+                           ->getMock();
+        $modelMock2->expects($this->once())
+                   ->method('ping')
+                   ->will($this->returnValue(true));
+        $modelMock2->expects($this->once())
+                   ->method('countUsers')
+                   ->with($this->equalTo($options))
+                   ->will($this->returnValue(3));
+
+        $this->model->addSecurityModel($modelMock);
+        $this->model->addSecurityModel($modelMock2);
+
+        $result = $this->model->countUsers($options);
+
+        $this->assertEquals($result, 5);
     }
 
 }
